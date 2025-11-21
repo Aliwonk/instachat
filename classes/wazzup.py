874f5ee57377eb9cecc6da799848fad3b2e5a6f9
AIdysh Baldan <aidysh0324@gmail.com>
2025-11-21 12:23:08 +0700

    feat: снова добавлен requests

--- classes/wazzup.py
+++ classes/wazzup.py
@@ -1,11 +1,13 @@
-import httpx
+import requests
 
 
 class Wazzup:
     def __init__(self, api_key, url):
         self.URL = url
         self.API_KEY = api_key
-        print("Создан экземпляр класса для работы с Wazzup API")
+        print("Создан экземепляр класса для работы с Wazzup API")
+
+    # METHODS
 
     def send_message(self, options):
         url = f"{self.URL}/message"
@@ -23,8 +25,7 @@ class Wazzup:
         }
 
         try:
-            with httpx.Client() as client:
-                response = client.post(url, json=payload, headers=headers)
+            response = requests.post(url, json=payload, headers=headers)
 
             if response.status_code == 200:
                 print("Сообщение успешно отправлено")
@@ -40,33 +41,30 @@ class Wazzup:
             return None
 
     def get_channels(self):
-        try:
-            with httpx.Client() as client:
-                response = client.get(
-                    f"{self.URL}/channels",
-                    headers={
-                        "Authorization": f"Bearer {self.API_KEY}",
-                        "Content-Type": "application/json",
-                    },
-                )
-            if response.status_code == 200:
-                return response.json()
-            else:
-                print(f"Произошла ошибка: {response.status_code} - {response.text}")
-                return None
-        except Exception as e:
-            print(f"Ошибка при получении каналов: {e}")
-            return None
+        response = requests.get(
+            f"{self.URL}/channels",
+            headers={
+                "Authorization": f"Bearer {self.API_KEY}",
+                "Content-Type": "application/json",
+            },
+        )
+        if response.status_code == 200:
+            return response.json()
+        else:
+            print(f"Прооизошла ошибка: {response.status_code} - {response.text}")
 
     def get_channel_by_id(self, channel_id):
         channels = self.get_channels()
-        if channels:
-            for channel in channels:
-                print(channel)
-                if channel.get("channelId") == channel_id:
-                    return channel
-        return None
+        for channel in channels:
+            print(channel)
+            if channel.get("channelId") == channel_id:
+                return channel
+            else:
+                return None
 
     # GETTERS
+
     def get_params_API(self):
         return {"URL": self.URL, "API_KEY": self.API_KEY}
+
+    # SETTERS
