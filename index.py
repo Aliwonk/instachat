d6bf74ea42578379ba030d20ae146f451c1660e8
AIdysh Baldan <aidysh0324@gmail.com>
2025-11-21 12:49:45 +0700

    fix: исправлен маршрут подключения webhook

--- index.py
+++ index.py
@@ -1,15 +1,16 @@
 import fastapi
-# import os
-# import dotenv
+from fastapi import Body
+import os
+import dotenv
 from classes.wazzup import Wazzup
 
-# dotenv.load_dotenv()
+dotenv.load_dotenv()
 
 app = fastapi.FastAPI()
 
 # Инициализируем Wazzup только если переменные окружения существуют
-wazzup_api_key = "64a39a928304492f89df4f7f8b16f692" # os.getenv("WAZZUP_API_KEY")
-wazzup_api_url = "https://api.wazzup24.com/v3" # os.getenv("WAZZUP_API_URL")
+wazzup_api_key = os.getenv("WAZZUP_API_KEY")
+wazzup_api_url = os.getenv("WAZZUP_API_URL")
 
 if wazzup_api_key and wazzup_api_url:
     wazzup = Wazzup(wazzup_api_key, wazzup_api_url)
@@ -24,22 +25,30 @@ def main():
 
 
 @app.post("/sub", status_code=200)
-def response_sub(data: dict = fastapi.Body()):
-    if wazzup is None:
-        return fastapi.responses.PlainTextResponse(
-            content="Wazzup не настроен", status_code=500
-        )
+def response_sub(data=Body()):
+    if data:
+        if wazzup is None:
+            return fastapi.responses.PlainTextResponse(
+                content="Wazzup не настроен", status_code=500
+            )
 
-    options = {}
-    message = data["messages"][0]
-    if message.get("isEcho") == False:
-        options["channelId"] = message["channelId"]
-        options["chatType"] = message["chatType"]
-        options["chatId"] = message["chatId"]
-        options["text"] = (
-            "Здравствуйте! Наши консультанты будут рады ответить на все ваши вопросы в Телеграм чате! https://t.me/+ETTFVnzsHMY2NTBi"
-        )
-        wazzup.send_message(options)
+        options = {}
+        if "messages" not in data:
+            return fastapi.responses.JSONResponse(
+                content={"message": "ok"}, status_code=200
+            )
+        
+        message = data["messages"][0]
+        if message.get("isEcho") == False:
+            options["channelId"] = message["channelId"]
+            options["chatType"] = message["chatType"]
+            options["chatId"] = message["chatId"]
+            options["text"] = (
+                "Здравствуйте! Наши консультанты будут рады ответить на все ваши вопросы в Телеграм чате! https://t.me/+ETTFVnzsHMY2NTBi"
+            )
+            wazzup.send_message(options)
 
-    print(f"Уведомление: {data}")
-    return fastapi.responses.PlainTextResponse(content="Подключение webhook")
+        print(f"Уведомление: {data}")
+        return fastapi.responses.JSONResponse(
+            content={"message": "ok"}, status_code=200
+        )
